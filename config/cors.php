73cb1b4b130a7b4a450a7e6f28288f783cd0c006
reposcon <cancerbero918@gmail.com>
2026-02-23 14:53:07 -0500

    se ajusta ruta definitiva para el manejo de local y produccion

--- config/cors.php
+++ config/cors.php
@@ -1,5 +1,23 @@
 <?php
 
+// return [
+//     'paths' => ['api/*', 'login', 'logout', 'sanctum/csrf-cookie'],
+
+//     'allowed_methods' => ['*'],
+
+//     'allowed_origins' => ['http://localhost:4200'],
+
+//     'allowed_origins_patterns' => [],
+
+//     'allowed_headers' => ['*'],
+
+//     'exposed_headers' => [],
+
+//     'max_age' => 0,
+
+//     'supports_credentials' => true,
+// ];
+
 return [
 
     'paths' => ['api/*', 'login', 'logout', 'sanctum/csrf-cookie'],
